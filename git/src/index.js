1c3a20f02d13662e4ae1201359e9d0c02167d593
Muhammad Hassan <92513804+Hassan786110@users.noreply.github.com>
2022-01-29 19:17:56 +0500

    work complete

---
+++ git/src/index.js
@@ -0,0 +1,36 @@
+import react from "react";
+import reactDom from "react-dom";
+
+reactDom.render(
+  <>
+    <h1>Muhammad Hassan</h1>
+    <br />
+    <ol>
+      <li><h2>Personal Info</h2>
+      <ul>
+        <li>Phone Number: 03362298975</li>
+        <li>Email Address: muhammmadhasssan110786@gmail.com</li>
+      </ul>
+      </li>
+      <li><h2>Qualification</h2>
+      <ul>
+        <li>2021 - Matriculation from Computer Science, Al-Murtaza School</li>
+      </ul>
+      </li>
+      <li><h2>Skills</h2>
+      <ul>
+        <li>HTML 5</li>
+        <li>CSS 3</li>
+        <li>JavaScript ES6</li>
+      </ul>
+      </li>
+      <li><h2>Languages</h2>
+        <ul>
+          <li>Urdu</li>
+        </ul>
+      </li>
+    </ol>
+  </>
+  ,
+  document.getElementById("root")
+)
